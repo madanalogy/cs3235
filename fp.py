d3dc0888a0ba5529d8d3025b780540bd6cf122ca
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-11-11 00:26:42 +0800

    Second Implementation

--- fp.py
+++ fp.py
@@ -7,9 +7,10 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 
 raw = ['time', 'size', 'direction']
-features = ['in_ratio', 'out_ratio', 'avg_sz', 'avg_sz_out', 'avg_sz_in', 'freq', 'freq_in', 'freq_out']
+features = ['left_in', 'mid_in',  'right_in', 'left_out', 'mid_out', 'right_out']
+label = 'label'
 cols = features.copy()
-cols.append('label')
+cols.append(label)
 
 
 def delta(d):
@@ -24,78 +25,63 @@ def load(directory='traces/profile', n=35, k=8):
     data = []
     for i in range(k):
         for j in range(n):
+            in_data = []
+            out_data = []
             with open(directory + str(i + 1) + '/' + str(j + 1)) as fp:
                 for line in fp:
                     time, size, dir = line.split()
                     hrs, mins, secs = time.split(':')
                     secs, micro = secs.split('.')
                     size = int(size)
-                    d = datetime.timedelta(
-                        hours=int(hrs), minutes=int(mins), seconds=int(secs), microseconds=int(micro)
-                    )
-                    data.append([d, size, dir])
-    return pd.DataFrame(np.array(data), columns=raw)
-
-
-def extract(directory='traces/profile', n=35, k=8):
-    data = []
-    for i in range(k):
-        for j in range(n):
-            num_out = 0
-            num_in = 0
-            sz_total = 0
-            sz_in = 0
-            sz_out = 0
-            d_in = 0
-            d_out = 0
-            d_total = 0
-            with open(directory + str(i + 1) + '/' + str(j + 1)) as fp:
-                lines = list(fp.readlines())
-                length = len(lines)
-                last_in = None
-                last_out = None
-                deltas = []
-                for idx, line in enumerate(lines):
-                    time, size, dir = line.split()
-                    hrs, mins, secs = time.split(':')
-                    secs, micro = secs.split('.')
-                    size = int(size)
-                    sz_total += size
-                    deltas.append(datetime.timedelta(
-                        hours=int(hrs), minutes=int(mins), seconds=int(secs), microseconds=int(micro)))
-                    if dir == 'in':
-                        num_in += 1
-                        sz_in += size
-                        if last_in is not None:
-                            d_in += delta(deltas[idx] - last_in)
-                        last_in = deltas[idx]
-                    elif dir == 'out':
-                        num_out += 1
-                        sz_out += size
-                        if last_out is not None:
-                            d_out += delta(deltas[idx] - last_out)
-                        last_out = deltas[idx]
-                if length != 0:
-                    d_total = delta(deltas[length - 1])
-            data.append([
-                divide(num_in, length),
-                divide(num_out, length),
-                divide(sz_total, length),
-                divide(sz_in, num_in),
-                divide(sz_out, num_out),
-                divide(d_total, length),
-                divide(d_in, num_in),
-                divide(d_out, num_out),
-                j + 1
-            ])
+                    if size != 0:
+                        d = datetime.timedelta(
+                            hours=int(hrs), minutes=int(mins), seconds=int(secs), microseconds=int(micro)
+                        )
+                        if dir == 'in':
+                            in_data.append((delta(d), size))
+                        else:
+                            out_data.append((delta(d), size))
+            data.append(extract(in_data, out_data, j + 1))
     return pd.DataFrame(np.array(data), columns=cols)
 
 
-def evaluate(split=0.1, metric='distance', k=5):
-    data = extract()
+def extract(in_data, out_data, target):
+    left_in, mid_in, right_in, in_total = gradients(in_data)
+    left_out, mid_out, right_out, out_total = gradients(out_data)
+    return [left_in, mid_in, right_in, left_out, mid_out, right_out, target]
+    # divide(in_total, out_total), divide(len(in_data), len(out_data)), target]
+
+
+def gradients(data):
+    left = 0
+    mid = 0
+    right = 0
+    total_s = 0
+    total_d = 0
+    total = 0
+    for i, (d, s) in enumerate(data):
+        total += s
+        total_s += s
+        total_d += d
+        if i == int(len(data) / 3):
+            left = divide(total_s, total_d)
+            total_s = 0
+            total_d = 0
+        elif i == int(len(data) * 2 / 3):
+            mid = divide(total_s, total_d)
+            total_s = 0
+            total_d = 0
+        elif i == len(data) - 1:
+            right = divide(total_s, total_d)
+            total_s = 0
+            total_d = 0
+    return left, mid, right, total
+
+
+def evaluate(data, split=35, metric='distance', k=7):
     X = preprocessing.normalize(data[features])
-    y = data['label']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split)
+    y = data[label]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split, shuffle=False)
 
     neigh = KNeighborsClassifier(n_neighbors=k, weights=metric)
     neigh.fit(X_train, y_train)
@@ -103,4 +89,4 @@ def evaluate(split=0.1, metric='distance', k=5):
 
 
 if __name__ == '__main__':
-    pass
+    print(evaluate(load()))
